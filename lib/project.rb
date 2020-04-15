5dfd29631f4bdc2ceccaefb5f45a3663a2dd24b3
Zoey Chesny <z.chesny@gmail.com>
2020-04-15 00:21:16 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -4,8 +4,12 @@ class Project
   attr_reader :backers, :title
 
   def initialize(title)
-    @title = title 
+    @title = title
     @backers = []
+  end
+
+  def add_backer(backer)
+    self.backers << backer
   end
 
 end
